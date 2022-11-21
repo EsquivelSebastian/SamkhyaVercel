92cb719d2658f66b36fb898fc01290e614364df2
EsquivelSebastian <esquivel.sebastian@me.com>
2022-11-20 22:39:46 -0300

    working on responsive navBar

--- js/script.js
+++ js/script.js
@@ -1,17 +1,10 @@
 document.getElementById("icon_menu").addEventListener("click", show_menu);
 
 function show_menu(){
-    document.querySelector(".menu").classList.toggle("drop_down_menu")
+    document.querySelector("menu__container").classList.toggle("none")
+
+
 }
 
 
 
-window.onscroll = function(){
-    var  position = window.pageYOffset || document.documentElement.scrollTop;
-    var element1 = document.getElementById("icon_heart");
-    var element2 = document.getElementById("icon_fire");
-    element1.style.bottom = position * 0.1 + "px";
-    element1.style.top = position * 0.1 + "px"; 
-    element2.style.bottom = position * 0.1 + "px";
-    element2.style.top = position * 0.1 + "px"; 
-}
