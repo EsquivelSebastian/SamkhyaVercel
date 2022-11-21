803534990a44ca82cc61535037a7b87dfd0b8994
EsquivelSebastian <esquivel.sebastian@me.com>
2022-11-21 06:19:03 -0300

    working on responsive navBar

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 document.getElementById("icon_menu").addEventListener("click", show_menu);
 
 function show_menu(){
-    document.querySelector("menu__container").classList.toggle("none")
+    document.querySelector(".menu__links").classList.toggle("none")
 
 
 }
