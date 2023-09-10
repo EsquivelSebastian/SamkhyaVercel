17499ff1a054ee36234dbff4e6be01a86392b0b9
EsquivelSebastian <esquivel.sebastian@me.com>
2023-09-10 11:40:59 -0300

    Trabajo de Footer

--- js/script.js
+++ js/script.js
@@ -5,3 +5,21 @@ function show_menu(){
 
 
 }
+
+/* Captura conducta de scroll-down para cambiar clases  */
+
+/* document.addEventListener('DOMContentLoaded', function() {
+    let scrolledDown = false; // Variable de estado para controlar si se ha hecho scroll hacia abajo
+
+    window.addEventListener('scroll', function() {
+        if (!scrolledDown) {
+            // Cambiar la clase del DIV aquí
+            const miDiv = document.getElementById('miDiv'); // Reemplaza 'miDiv' con el ID de tu DIV
+            miDiv.classList.add('nuevaClase'); // Reemplaza 'nuevaClase' con la clase que deseas agregar
+
+            // Actualizar la variable de estado
+            scrolledDown = true;
+        }
+    });
+});
+ */
